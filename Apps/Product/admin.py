6b79899c56b4e991b8c42d2e080c1f5724f864d3
Paul <paulXmbingu@gmail.com>
2025-05-27 02:08:36 +0300

    UPdate

--- Apps/Product/admin.py
+++ Apps/Product/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import DetailModel
+from .models import DetailModel, CategoryModel
+
 
 @admin.register(DetailModel)
 class DetailModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sku_code', 'price', 'currency')
-    search_fields = ('name', 'sku_code')
-    list_filter = ('currency',)
-    ordering = ('name',)
-    readonly_fields = ()
+    list_display = ('name','sku_code', 'currency', 'price')
+    search_fields = ('name', 'brand', 'sku_code',)
     
+    
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('category', 'sub_category')
+    search_fields = ('category', 'sub_category')
