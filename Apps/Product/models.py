6550468a98280044df31c2198c49afa3dd635230
Marion Ngayi <marionngayi@Marions-MacBook-Air.local>
2025-05-27 03:05:06 +0300

    Update

--- Apps/Product/models.py
+++ Apps/Product/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# DETAIL MODEL
+
+''' DETAIL MODEL '''
 class DetailModel(models.Model):
-    '''Product Details Model. Contains; Name, Brand, SKU Code, AI Summary Description, Currency, & Price'''
     name = models.CharField(max_length=255, verbose_name='Name', null=False, blank=False)
     brand = models.CharField(max_length=255, verbose_name='Brand', null=False, blank=False)
     category = models.ForeignKey('CategoryModel', on_delete=models.CASCADE, related_name='products', null=True, blank=False)
+    sub_category = models.ForeignKey('SubCategoryModel', on_delete=models.CASCADE, related_name='products', null=True, blank=False)
     sku_code = models.CharField(max_length=255, verbose_name='SKU Code', null=False, blank=False)
     product_summary = models.TextField(verbose_name='AI Summary Description', null=False, blank=False)
     currency = models.CharField(max_length=255, verbose_name='Currency', null=False, blank=False, default='KES')
@@ -20,14 +21,26 @@ class DetailModel(models.Model):
     
 
 
-# CATEGORY MODEL
+'''CATEGORY MODEL '''
 class CategoryModel(models.Model):
-    category = models.CharField(max_length=255, verbose_name='Category', null=False, blank=False)
-    sub_category = models.CharField(max_length=255, verbose_name='Sub Category', null=False, blank=False)
+    category = models.CharField(max_length=255, verbose_name='Category', null=False, blank=False)    
     
     class Meta:
         verbose_name = 'Category Model'
         verbose_name_plural = 'Category Models'
 
     def __str__(self):
-        return self.category + '-' + self.sub_category
+        return self.category
+    
+
+
+'''SUBCATEGORY MODEL '''
+class SubCategoryModel(models.Model):
+    sub_category = models.CharField(max_length=255, verbose_name='Sub Category', null=False, blank=False)
+    
+    class Meta:
+        verbose_name = 'Sub Category Model'
+        verbose_name_plural = 'Sub Category Models'
+
+    def __str__(self):
+        return self.sub_category
