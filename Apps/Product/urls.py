4eddbe9555f6d2c60700453e1d496f40c7c374df
Paul Mbingu <paulXmbingu@gmail.com>
2025-06-01 12:27:56 +0300

    Update

--- Apps/Product/urls.py
+++ Apps/Product/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import (DetailModelListView, DetailModelCreateView, DetailModelRetrieveView, DetailModelUpdateView, DetailModelDestroyView,)
 
 urlpatterns = [
-    path('details/', DetailModelListView.as_view(), name='detail-list'),
-    path('details/create/', DetailModelCreateView.as_view(), name='detail-create'),
-    path('details/<int:pk>/', DetailModelRetrieveView.as_view(), name='detail-retrieve'),
-    path('details/<int:pk>/update/', DetailModelUpdateView.as_view(), name='detail-update'),
-    path('details/<int:pk>/delete/', DetailModelDestroyView.as_view(), name='detail-delete'),
+    path('details/', DetailModelListView.as_view(), name='detail_list'),
+    path('details/create/', DetailModelCreateView.as_view(), name='detail_create'),
+    path('details/<int:pk>/', DetailModelRetrieveView.as_view(), name='detail_retrieve'),
+    path('details/<int:pk>/update/', DetailModelUpdateView.as_view(), name='detail_update'),
+    path('details/<int:pk>/delete/', DetailModelDestroyView.as_view(), name='detail_delete'),
 ]
