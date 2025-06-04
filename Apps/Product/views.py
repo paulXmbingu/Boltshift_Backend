b4a651ea75b0fd617b2f039809a340321bfd99b6
Paul Mbingu <paulXmbingu@gmail.com>
2025-06-05 02:17:01 +0300

    Update

--- Apps/Product/views.py
+++ Apps/Product/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics
 from .models import (
-    DetailModel, 
+    DetailModel, SummaryDescriptionModel, 
     )
 from .serializers import (
-    DetailModelSerializer, 
+    DetailModelSerializer, SummaryDescriptionSerializer, 
     )
 
 
@@ -29,3 +29,28 @@ class DetailUpdateView(generics.UpdateAPIView):
 class DetailDestroyView(generics.DestroyAPIView):
     queryset = DetailModel.objects.all()
     serializer_class = DetailModelSerializer
+
+
+
+
+
+'''SUMMARY DESCRIPTION VIEW'''
+class SummaryDescriptionListView(generics.ListAPIView):
+    queryset = SummaryDescriptionModel.objects.all()
+    serializer_class = SummaryDescriptionSerializer
+
+class SummaryDescriptionCreateView(generics.CreateAPIView):
+    queryset = SummaryDescriptionModel.objects.all()
+    serializer_class = SummaryDescriptionSerializer
+
+class SummaryDescriptionRetrieveView(generics.RetrieveAPIView):
+    queryset = SummaryDescriptionModel.objects.all()
+    serializer_class = SummaryDescriptionSerializer
+
+class SummaryDescriptionUpdateView(generics.UpdateAPIView):
+    queryset = SummaryDescriptionModel.objects.all()
+    serializer_class = SummaryDescriptionSerializer
+
+class SummaryDescriptionDestroyView(generics.DestroyAPIView):
+    queryset = SummaryDescriptionModel.objects.all()
+    serializer_class = SummaryDescriptionSerializer
